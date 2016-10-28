4d3a91b8f767b764bdb2d3a6c99315a34ddabe34
Andrew Duncan <abduncan0@gmail.com>
2016-10-28 13:46:59 -0400

    comments and such

--- Company.API/Company.Domain/Repository/UserRepository/UserRepository.cs
+++ Company.API/Company.Domain/Repository/UserRepository/UserRepository.cs
@@ -11,17 +11,27 @@ namespace Company.Domain.Repository.UserRepository
 {
     public class UserRepository : IUserRepository
     {
+        // Private backing variable to hold the reference
+        // to the injected mongoDb.
         private readonly IMongoDatabase _db;
 
+        // Create a new instance of the UserRepository.
         public UserRepository(IMongoDatabase db)
         {
+            // Store the reference to the inject IMongoDatabase
+            // instance.
             _db = db;
         }
 
         public User FindUserByEmail(string email)
         {
+            // Get a reference to the users collection in the database.
             var userCollection = _db.GetCollection<User>("users");
+
+            // Create a filter used to filter the collection.
             var filter = Builders<User>.Filter.Eq("email", email);
+
+            // Execute the query.
             return userCollection.Find(filter).FirstOrDefault();
         }
     }
